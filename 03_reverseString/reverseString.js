27f4bb43aaa97eb0ab1ab663c7cab0c7d7058625
fuelCoffee <fuelcoffee@users.noreply.github.com>
2023-11-07 20:56:36 -0600

    Exercise #3 - Reverse a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  // Step 1. Use the split() method to return a new array
+  let splitStr = string.split("");
+  // Step 2. Use the reverse() method to reverse the new created array
+  let reverseArray = splitStr.reverse();
+  // Step 3. Use the join() method to join all elements of the array into a string
+  let joinArr = reverseArray.join("");
+  //Step 4. Return the reversed string
+  return joinArr;
 };
 
 // Do not edit below this line
