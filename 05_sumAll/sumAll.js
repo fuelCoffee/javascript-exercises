e7fc8cc7263bfb628598d8b708a0af021dd1acf1
fuelCoffee <fuelcoffee@users.noreply.github.com>
2023-11-08 21:50:52 -0600

    Exercises 4-7 - JS Fundamentals, Part 4
    Used rest argument in a function to remove any number of arguments
    from an array. Summed all numbers between two function parameters.
    Wrote a function to return only leap years.
    Another function to do temperature conversion.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function (firstN, secondN) {
+  //loop: start at first number and continue adding to finalSum until reaching second number
+  let finalSum = 0;
+  let errorMessage = "ERROR";
+  if (firstN !== +firstN || secondN !== +secondN) {
+    return errorMessage;
+  } else if (firstN < 0 || secondN < 0) {
+    return errorMessage;
+  } else {
+    if (firstN > secondN) {
+      [firstN, secondN] = [secondN, firstN];
+    }
+    for (i = firstN; i <= secondN; i++) {
+      finalSum += i;
+    }
+    return finalSum;
+  }
 };
-
 // Do not edit below this line
 module.exports = sumAll;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  const tempConversion = (temp - 32) * (5 / 9);
+  //Round to one decimal place
+  return Math.round(tempConversion * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  const tempConversion = temp * (9 / 5) + 32;
+  //Round to one decimal place
+  return Math.round(tempConversion * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
