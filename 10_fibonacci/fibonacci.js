280c52c6ac10dde58ef7a8fbc0ca9f71ba3e7f74
fuelCoffee <fuelcoffee@users.noreply.github.com>
2024-03-30 15:41:26 -0500

    Complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  let result = parseInt(num);
 
+  if (Math.sign(result) !== -1) {
+    //Create variables to run Fibonacci
+    let n1 = 0,
+      n2 = 1,
+      nextTerm;
+    //Loop until
+    for (let i = 1; i <= result; i++) {
+      nextTerm = n1 + n2;
+      n1 = n2;
+      n2 = nextTerm;
+    }
+    result = n1;
+  } else {
+    return "OOPS";
+  }
+  return result;
 };
 
 // Do not edit below this line
